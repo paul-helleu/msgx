df6a79a11a31b3a1f6f1ae3feed3190ea0b665aa
paul-helleu <paul.helleu@orange.fr>
2025-05-27 22:34:55 +0200

    fix: socket only get message from specifics rooms

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,29 +1,25 @@
 import { Server } from "socket.io";
-import type { IMessage } from "./interfaces/IMessage";
-import Message from "./database/models/Message";
-import conversationRepository from "./database/repositories/conversation.repository";
-import userRepository from "./database/repositories/user.repository";
 
 const port = Number(process.env.SERVER_SOCKET_PORT) || 3300;
 const io = new Server(port, {
   cors: { origin: process.env.SERVER_CLIENT_URI! },
 });
 
-const getSocketCode = (receiver: string) => receiver;
-const getConversationId = () => 1;
-
 io.on("connection", (socket) => {
   socket.on("joinChannel", (channelId) => {
     socket.join(channelId);
   });
 
-  socket.on("sendMessage", ({ channelId, message, senderId }) => {
-    const payload = {
-      channelId,
-      senderId,
-      message,
-    };
-    io.to(channelId).emit("newMessage", payload);
+  socket.on("message", ({ channelId, message, senderId }) => {
+    if (socket.rooms.has(channelId)) {
+      const payload = {
+        channelId,
+        senderId,
+        message,
+      };
+      io.to(channelId).emit("message", payload);
+    } else {
+      socket.emit("error", "You are not in this room");
+    }
   });
-  // socket.on("channels/1", (msg: IMessage) => {});
 });
