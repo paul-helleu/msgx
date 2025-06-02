17b42b6ff5aba79a7cd709af78b969e0be6bfda0
Paul BUCHON <106670957+BuchonPaul@users.noreply.github.com>
2025-06-03 00:03:21 +0200

    feat: logout button

--- client/src/components/ProfileFooter.tsx
+++ client/src/components/ProfileFooter.tsx
@@ -1,6 +1,7 @@
 import type { User } from '../interfaces/User';
 import ProfilePicture from './ProfilePicture';
 import { BiRegularMessageAdd } from 'solid-icons/bi';
+import { FiLogOut } from 'solid-icons/fi';
 export default function ProfileFooter(props: { user: User | null }) {
   return (
     <div class="bg-gray-200 p-4 flex items-center justify-between">
@@ -13,13 +14,20 @@ export default function ProfileFooter(props: { user: User | null }) {
           {props.user?.username ?? 'Utilisateur'}
         </div>
       </div>
-
-      <BiRegularMessageAdd
-        onClick={() => {
-          console.log('Ajouter une conversation');
-        }}
-        class="text-2xl hover:text-indigo-600 text-gray-600"
-      />
+      <div class="flex">
+        <FiLogOut
+          onClick={() => {
+            console.log('Déconnexion');
+          }}
+          class="text-2xl hover:text-red-500 text-red-700 ml-6"
+        />
+        <BiRegularMessageAdd
+          onClick={() => {
+            console.log('Ajouter une conversation');
+          }}
+          class="text-2xl hover:text-indigo-600 text-gray-600 ml-6"
+        />
+      </div>
     </div>
   );
 }
