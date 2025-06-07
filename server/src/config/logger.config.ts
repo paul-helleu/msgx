df87e4f38c588c6d23cb5ddb9af69ae9172969ff
paul-helleu <paul.helleu@orange.fr>
2025-06-08 01:24:01 +0200

    feat: logger

---
+++ server/src/config/logger.config.ts
@@ -0,0 +1,22 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+          }),
+        ]
+      : []),
+  ],
+});
--- server/src/database/sequelize.ts
+++ server/src/database/sequelize.ts
@@ -1,15 +1,28 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, type Dialect } from 'sequelize';
+import { logger } from '../config/logger.config';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: 'postgres',
-    logging: console.log,
-  }
-);
+const DB_NAME = process.env.DB_NAME as string;
+const DB_USER = process.env.DB_USER as string;
+const DB_PASSWORD = process.env.DB_PASSWORD as string;
+const DB_HOST = process.env.DB_HOST as string;
+const DB_PORT = Number(process.env.DB_PORT);
+const DB_DIALECT = process.env.DB_DIALECT as Dialect;
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: DB_DIALECT,
+  logging: (msg) => {
+    if (NODE_ENV === 'development') {
+      logger.debug(msg);
+    } else if (NODE_ENV === 'production') {
+      logger.info(msg);
+    } else if (NODE_ENV === 'test') {
+      // Silence
+    }
+  },
+});
 
 export default sequelize;
